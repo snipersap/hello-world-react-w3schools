b51c0a9488429f05506d239207c53a0e60a96800
sjyoti <soumyojyoti.pal@honeywell.com>
2022-01-27 00:03:43 +0530

    Initial commit

---
+++ hello-world/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create my first element and render it
+const myFirstElement = <h1>Hello React!</h1>
+ReactDOM.render(myFirstElement, document.getElementById("root"));
+
