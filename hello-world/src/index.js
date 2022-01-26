9884dc44877cf998708efc7b99308b3b52c65b23
sjyoti <soumyojyoti.pal@honeywell.com>
2022-01-27 00:11:40 +0530

    created my first class and obj

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//Create my first element and render it
+/*Create my first element and render it
 const myFirstElement = <h1>Hello React!</h1>
 ReactDOM.render(myFirstElement, document.getElementById("root"));
+*/
 
+
+//Create my first class
+class Car{
+  constructor(name){
+    this.brand = name;
+  }
+} 
+//Create an object of the car
+const myCar = new Car("Ford");
